2975bd5fd15824d7627e5390c060df77a0436880
ShotaOnishi <showta.0804@gmail.com>
2016-09-01 09:52:46 +0900

    questionsテーブル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :questions
+  root 'home#index'
+  resources :home
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
