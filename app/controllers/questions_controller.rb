8183025e8546c5058106b7d6c1db0983a88ebf28
えび (Satoshi Isemura) <satoshi.isemura@monstar-lab.com>
2016-09-01 22:26:37 +0900

    exec `rubocop --auto-correct`

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
@@ -62,13 +63,14 @@ class QuestionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_question
-      @question = Question.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def question_params
-      params.require(:question).permit(:title,:user_id,:content,:address)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def question_params
+    params.require(:question).permit(:title, :user_id, :content, :address)
+  end
 end
