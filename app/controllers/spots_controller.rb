4394ef8177a36262884866720308d3215ddfa2df
Ryoma-h777 <chocolate.rh19@ezweb.ne.jp>
2016-09-05 14:51:17 +0900

    Edit&Delete of spot was delete

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -38,29 +38,6 @@ class SpotsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /spots/1
-  # PATCH/PUT /spots/1.json
-  def update
-    respond_to do |format|
-      if @spot.update(spot_params)
-        format.html { redirect_to @spot, notice: 'Spot was successfully updated.' }
-        format.json { render :show, status: :ok, location: @spot }
-      else
-        format.html { render :edit }
-        format.json { render json: @spot.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /spots/1
-  # DELETE /spots/1.json
-  def destroy
-    @spot.destroy
-    respond_to do |format|
-      format.html { redirect_to spots_url, notice: 'Spot was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
 
