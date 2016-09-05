aeb6e6e5509d2e7669921523fa53ffc9a833a2e4
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-09-05 18:18:55 +0900

    :+1: Enable Bullet on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ### Bullet ###
+  Bullet.enable       = true
+  Bullet.alert        = true
+  Bullet.rails_logger = true
 end
