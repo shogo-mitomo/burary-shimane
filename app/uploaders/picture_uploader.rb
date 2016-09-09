8f8ac71eb3f317e18a209881c559a82884086da3
ShotaOnishi <showta.0804@gmail.com>
2016-09-09 12:10:59 +0900

    写真投稿バグ修正（環境によるupload houhouno）

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,11 +5,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  include Cloudinary::CarrierWave
-
-  process :convert => 'png'
-  process :tags => ['picture']
   storage :file
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+
+    process :convert => 'png'
+    process :tags => ['picture']
   # Choose what kind of storage to use for this uploader:
   # storage :fog
   version :standard do
@@ -25,9 +26,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
+end
+
+def store_dir
+  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
