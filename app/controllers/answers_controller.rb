7e9a16914e8e77c21ce48e381005d39640fb185a
ShotaOnishi <showta.0804@gmail.com>
2016-09-09 16:15:47 +0900

    最終バグ修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,7 +4,7 @@ class AnswersController < ApplicationController
   # GET /answers
   # GET /answers.json
   def index
-    @answers = Answer.all
+    @answers = Answer.all.order("id DESC")
   end
 
   # GET /answers/1
